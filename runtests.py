faa40b1927585bc95341a8909fa03eb7a636393e
Axel Goetz <axel.goetz@hotmail.com>
2016-03-02 12:50:12 +0000

    Colored loading bars

--- runtests.py
+++ runtests.py
@@ -1,10 +1,13 @@
 import subprocess
-import time
 import os
+import sys
 
+rootDirectoy = os.path.join(os.path.expanduser('~'),'Projects/Compilers')
+BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE = range(8)
 
 def getFileNames():
-    files = [val for sublist in [[os.path.join(i[0], j) for j in i[2]] for i in os.walk('./tests')] for val in sublist]
+    print(rootDirectoy + '/tests')
+    files = [val for sublist in [[os.path.join(i[0], j) for j in i[2]] for i in os.walk(rootDirectoy + '/tests')] for val in sublist]
     negatives = []
     positives = []
     for file in files:
@@ -16,32 +19,65 @@ def getFileNames():
 
 
 def runtests(files, positive):
-    testfailed = 0
-    for file in files:
-        string = 'bin/:lib/java-cup-11b-runtime.jar'
+    testfailed = []
+    if(positive):
+        print("Testing positive files")
+    else:
+        print("Testing negative files")
 
-        p = subprocess.Popen(['java', '-cp', string, 'SC', file], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        err, out = p.communicate()
+    string = rootDirectoy + '/bin/:lib/java-cup-11b-runtime.jar'
+    progress = 1.0
+    total = len(files)
+
+    for file in files:
+        percentage = int((progress/total)*100.0)
+        spaces = ' ' * (100 - percentage)
+        p = subprocess.run(['java', '-cp', string, 'SC', file], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        err, out = (p.stderr, p.stdout)
         if err == '':
             if positive:
                 if "error" in out:
-                    print "Failed test :("
-                    print "Run the test again: java -cp " + string + " SC " + file
-                    testfailed += 1
+                    testfailed.append(file)
             else:
                 if "error" not in out:
-                    print "Failed test :("
-                    print "Run the test again: java -cp " + string + " SC " + file
-                    testfailed += 1
+                    testfailed.append(file)
+        progress+=1
+
+        if(percentage < 33):
+            sys.stdout.write('\rPercent: [' + '\x1b[1;%dm' % (30+RED) + '{0}{1}'.format('#'*percentage, spaces) + '\x1b[1;%dm' % (30+WHITE) +'] {0}%'.format(percentage))
+        elif(percentage < 66):
+            string = '\rPercent: [' + '\x1b[1;%dm' % (30+RED) + '{0}'.format('#'*33)
+            string += '\x1b[1;%dm' % (30+YELLOW) + '{0}{1}'.format('#'*(percentage - 33), spaces)
+            string += '\x1b[1;%dm' % (30+WHITE) + '] {0}%'.format(percentage)
+            sys.stdout.write('{0}'.format(string))
+        else:
+            string = '\rPercent: [' + '\x1b[1;%dm' % (30+RED) + '{0}'.format('#'*33)
+            string += '\x1b[1;%dm' % (30+YELLOW) + '{0}'.format('#'*33)
+            string += '\x1b[1;%dm' % (30+GREEN) + '{0}{1}'.format('#'*(percentage - 66), spaces)
+            string += '\x1b[1;%dm' % (30+WHITE) + '] {0}%'.format(percentage)
+            sys.stdout.write('{0}'.format(string))
+        sys.stdout.flush()
+    print(' ')
     return testfailed
 
+def printFailedTests(testfailed):
+    string = rootDirectoy + '/bin/:lib/java-cup-11b-runtime.jar'
+    for file in testfailed:
+        sys.out.write('\x1b[1;%dm' % (30+RED) + "Test failed :(\n" + '\x1b[1;%dm' % (30+WHITE))
+        print("Run the test again: java -cp " + string + " SC " + file)
+
+    if len(testfailed) == 0:
+        sys.stdout.write('\x1b[1;%dm' % (30+GREEN) + "Passed all of the tests, congrats :)\n")
+
 if __name__ == '__main__':
-    print "This might take a while..."
-    subprocess.Popen(['make'])
+    sys.stdout.write('\x1b[1;%dm' % (30+WHITE))
+    print("This might take a while...")
+
+    #subrocess.call() is blocking, need to wait until make finishes executing
+    subprocess.call(['make', '-C', rootDirectoy])
     files = getFileNames()
 
     testfailed = runtests(files['negatives'], False)
     testfailed += runtests(files['positives'], True)
 
-    if testfailed == 0:
-        print "Passed all of the tests, congrats :)"
+    printFailedTests(testfailed)
